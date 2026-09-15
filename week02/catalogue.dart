44fa8b6c15dc9161f9341bc0d39b20afa57d3aa3
jstnroxx <ms325358@gmail.com>
2026-09-15 22:26:14 +0500

    feat: complete level 4 task (week02)

--- week02/catalogue.dart
+++ week02/catalogue.dart
@@ -32,4 +32,19 @@ class Library {
 
     return _cachedReport;
   }
+
+  List<String> get allTitles => items.map((item) => item.title).toList();
+
+  List<Book> get recentBooks => items.whereType<Book>().where((book) => book.year > 2010).toList();
+
+  int get averagePages => (items.whereType<Book>().length > 0) ? (items.whereType<Book>().fold(0, (totalPages, book) => totalPages + book.pages) ~/ items.whereType<Book>().length) : 0;
+  // We don't use reduce here because we need to have initial value specified as 0 in order to accumulate pages.
+
+  Map<String, int> get authorStats => items.whereType<Book>().fold(<String, int>{}, (stats, book) => stats..[book.author.name] = (stats[book.author.name] ?? 0) + 1);
+
+  Set<String> get distinctAuthors => items.whereType<Book>().map((book) => book.author.name).toSet();
+
+  Set<Genre> get genresPresent => items.whereType<Book>().map((book) => book.genre).toSet();
+
+  List<String> get catalogueInfo => ['CATALOGUE', for (final book in items.whereType<Book>()) '${book.title} (${book.year})', ...items.whereType<Book>().map((book) => book.author.name).toSet(), if (items.whereType<Book>().any((book) => book.pages == 0)) '(incomplete data)'];
 }
